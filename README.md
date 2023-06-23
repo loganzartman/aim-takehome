130ce2723dd8a3272d6b65828e7a51a917c292a6
nondefault <spam@nondefault.net>
2023-06-23 13:37:51 -0700

    readme

---
+++ README.md
@@ -0,0 +1,16 @@
+## usage
+
+Dependencies: Node 16+, pnpm
+
+1. `cd src/client && pnpm install && pnpm start`
+2. `cd src/server && pnpm install && pnpm start`
+3. navigate to [localhost:1234](http://localhost:1234)
+
+## regenerating proto outputs
+
+Dependencies: Python 3, `protoc` and `protoc-gen-js` on path
+
+1. `cd proto`
+2. `python generate.py`
+
+Copyright 2023 Logan Zartman. All rights reserved.
--- proto/generate.py
+++ proto/generate.py
@@ -1,13 +1,15 @@
 import os
 import subprocess
-import shutil
 import tempfile
+import shutil
 
 def generate_js_code(proto_path, output_dir):
     with tempfile.TemporaryDirectory() as tmp_dir:
         command = f'protoc -I={proto_path} {os.path.join(proto_path, "*.proto")} --js_out=import_style=commonjs:{tmp_dir} --grpc-web_out=import_style=commonjs,mode=grpcwebtext:{output_dir}'
         subprocess.run(command, shell=True)
-        shutil.move(tmp_dir, output_dir)
+        if os.path.exists(output_dir):
+            shutil.rmtree(output_dir)
+        os.rename(tmp_dir, output_dir)
 
 if __name__ == "__main__":
     proto_path = '.'
