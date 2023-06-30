1f2797c0af2335d930fbaab83b210272c96383d6
nondefault <spam@nondefault.net>
2023-06-30 14:50:49 -0700

    correctly generate client proto code

--- proto/generate.py
+++ proto/generate.py
@@ -5,24 +5,21 @@ import shutil
 
 PROTOC_GEN_TS_PATH = os.path.join('.', 'node_modules', '.bin', 'protoc-gen-ts')
 
-def generate_code(proto_path, output_dir):
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        command = 'protoc'\
-            f' --plugin="protoc-gen-ts={PROTOC_GEN_TS_PATH}"'\
-            f' -I={proto_path}'\
-            f' --js_out="import_style=commonjs,binary:{tmp_dir}"'\
-            f' --grpc-web_out=mode=grpcwebtext:{tmp_dir}'''\
-            f' {os.path.join(proto_path, "*.proto")}'\
-            # f' --ts_out={tmp_dir}'\
-        # this line intentionally left blank
-        print('running:')
-        print(command)
-        subprocess.run(command, shell=True)
-        if os.path.exists(output_dir):
-            shutil.rmtree(output_dir)
-        os.rename(tmp_dir, output_dir)
+def generate_web(proto_path: str, output_dir: str):
+  with tempfile.TemporaryDirectory() as tmp_dir:
+    command = 'protoc'\
+      f' -I={proto_path}'\
+      f' {os.path.join(proto_path, "*.proto")}'\
+      f' --ts_out={tmp_dir}'\
+      # f' --grpc-web_out=mode=grpcwebtext:{tmp_dir}'''\
+      # f' --js_out="import_style=commonjs,binary:{tmp_dir}"'\
+    # this line intentionally left blank
+    print('running:', command)
+    subprocess.run(command, shell=True)
+    if os.path.exists(output_dir):
+      shutil.rmtree(output_dir)
+    os.rename(tmp_dir, output_dir)
 
 if __name__ == "__main__":
-    proto_path = '.'
-    output_dir = os.path.join('..', 'src', 'proto-generated')
-    generate_code(proto_path, output_dir)
+  proto_path = '.'
+  generate_web(proto_path, os.path.join('..', 'src', 'client', 'proto-generated'))
