0f7c65e19213e8a897f12e4adb0bdd9a8aa4582a
nondefault <spam@nondefault.net>
2023-06-30 19:31:25 -0700

    use ts codegen for server

--- proto/generate.py
+++ proto/generate.py
@@ -21,6 +21,21 @@ def generate_web(proto_path: str, output_dir: str):
       shutil.rmtree(output_dir)
     os.rename(tmp_dir, output_dir)
 
+def generate_node(proto_path: str, output_dir: str):
+  with tempfile.TemporaryDirectory() as tmp_dir:
+    command = 'protoc'\
+      f' -I={proto_path}'\
+      f' {os.path.join(proto_path, "*.proto")}'\
+      f' --ts_opt=target=node'\
+      f' --ts_out={tmp_dir}'\
+    # this line intentionally left blank
+    print('running:', command)
+    subprocess.run(command, shell=True)
+    if os.path.exists(output_dir):
+      shutil.rmtree(output_dir)
+    os.rename(tmp_dir, output_dir)
+
 if __name__ == "__main__":
   proto_path = '.'
   generate_web(proto_path, os.path.join('..', 'src', 'client', 'proto-generated'))
+  generate_node(proto_path, os.path.join('..', 'src', 'server', 'proto-generated'))
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,36 +1,37 @@
 import express from 'express';
 import * as grpc from '@grpc/grpc-js';
-import * as protoLoader from '@grpc/proto-loader';
+import {
+  Machine,
+  MachineStreamRequest,
+  UnimplementedMachineMapService,
+} from './proto-generated/service';
 
-const PROTO_PATH = '../../proto/service.proto';
-
-// Suggested options for similarity to existing grpc.load behavior
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-  keepCase: true,
-  longs: String,
-  enums: String,
-  defaults: true,
-  oneofs: true,
-});
-const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
-
-const Pause = () => {
-  throw new Error('Pause() unimplemented');
-};
-const UnPause = () => {
-  throw new Error('UnPause() unimplemented');
-};
-const MachineStream = () => {
-  throw new Error('MachineStream() unimplemented');
-};
+class MachineMapService extends UnimplementedMachineMapService {
+  Pause(
+    call: grpc.ServerUnaryCall<Machine, Machine>,
+    callback: grpc.sendUnaryData<Machine>
+  ): void {
+    throw new Error('Method not implemented.');
+  }
+  UnPause(
+    call: grpc.ServerUnaryCall<Machine, Machine>,
+    callback: grpc.sendUnaryData<Machine>
+  ): void {
+    throw new Error('Method not implemented.');
+  }
+  MachineStream(
+    call: grpc.ServerWritableStream<MachineStreamRequest, Machine>
+  ): void {
+    throw new Error('Method not implemented.');
+  }
+}
 
 const createServer = () => {
   const server = new grpc.Server();
-  server.addService((protoDescriptor as any).MachineMap.service, {
-    Pause,
-    UnPause,
-    MachineStream,
-  });
+  server.addService(
+    UnimplementedMachineMapService.definition,
+    new MachineMapService()
+  );
   return server;
 };
 
@@ -38,9 +39,7 @@ const routeServer = createServer();
 routeServer.bindAsync(
   '0.0.0.0:50051',
   grpc.ServerCredentials.createInsecure(),
-  () => {
-    routeServer.start();
-  }
+  () => routeServer.start()
 );
 
 const app = express();
