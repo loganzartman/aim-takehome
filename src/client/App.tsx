4d593676e891b5ae6214dd43ccbadb3f22643d06
nondefault <spam@nondefault.net>
2023-06-30 23:34:02 -0700

    basic leaflet integration

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,10 @@
 import {useCallback, useState} from 'react';
 import {Machine, MachineMapClient} from './proto-generated/service';
+import {MapContainer, TileLayer} from 'react-leaflet';
 
 const GRPC_ADDRESS = 'http://localhost:8080';
+const seattleLat = 47.60884551040699;
+const seattleLon = -122.34006911204551;
 
 export default function App() {
   const [client, setClient] = useState(
@@ -19,6 +22,19 @@ export default function App() {
     <div>
       <div>hello from react</div>
       <button onClick={handleUnPause}>UnPause</button>
+      <MapContainer
+        style={{width: '600px', height: '400px'}}
+        // @ts-ignore: Property 'center' does not exist; package types are incorrect
+        center={[seattleLat, seattleLon]}
+        zoom={15}
+        scrollWheelZoom
+      >
+        <TileLayer
+          // @ts-ignore: Property 'attribution' does not exist; package types are incorrect
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </div>
   );
 }
